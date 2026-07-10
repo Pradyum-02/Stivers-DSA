1ad1b059a6eec9b0ce1ed573186f0ef761339c34
Pradyum Meshram <pradyumofc13@gmail.com>
2026-07-10 23:37:28 +0530

    Time: 4 ms (22.78%), Space: 52.2 MB (83.62%) - LeetHub

---
+++ 0055-jump-game/0055-jump-game.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    bool canJump(vector<int>& nums) {
+        int mr = 0;
+
+        for (int i=0; i < nums.size(); i++){
+            if(i > mr){
+                return false;
+            }
+            mr = max(mr, i + nums[i]);
+        }
+        return true;
+    }
+};
