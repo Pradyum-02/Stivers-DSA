37681496a89c67aa81871b728bee2241297fff88
Pradyum Meshram <pradyumofc13@gmail.com>
2026-07-31 21:12:59 +0530

    Time: 0 ms (100%), Space: 28.2 MB (39.03%) - LeetHub

--- 0169-majority-element/0169-majority-element.cpp
+++ 0169-majority-element/0169-majority-element.cpp
@@ -1,9 +1,35 @@
+// class Solution {
+// public:
+//     int majorityElement(vector<int>& nums) {
+//         int n = nums.size();
+//         sort(nums.begin(), nums.end());
+//         return nums[n/2];
+        
+//     }
+// };
+
+
+
 class Solution {
 public:
     int majorityElement(vector<int>& nums) {
-        int n = nums.size();
-        sort(nums.begin(), nums.end());
-        return nums[n/2];
+        int cnt = 0;
+        int ele = 0;
+
+        for(int i=0; i<nums.size(); i++){
+            if(cnt == 0){
+                ele = nums[i];
+                cnt = 1;
+            }
+                else if (nums[i] == ele){
+                    cnt++;
+                }
+                else {
+                    cnt--;
+                }
+            }
+            return ele;
+        }
         
-    }
+    
 };
