1a42759bf70c55cb2d0ce873ed4f0fb6c06d3911
Pradyum Meshram <pradyumofc13@gmail.com>
2026-08-14 21:54:03 +0530

    Time: 0 ms (100%), Space: 13.7 MB (43.8%) - LeetHub

--- 0035-search-insert-position/0035-search-insert-position.cpp
+++ 0035-search-insert-position/0035-search-insert-position.cpp
@@ -1,7 +1,35 @@
+// class Solution {
+// public:
+//     int searchInsert(vector<int>& nums, int target) {
+//         return lower_bound(nums.begin(), nums.end(), target)-nums.begin();
+        
+//     }
+// };
+
+
+
 class Solution {
 public:
     int searchInsert(vector<int>& nums, int target) {
-        return lower_bound(nums.begin(), nums.end(), target)-nums.begin();
-        
+
+        int low = 0;
+        int n = nums.size();
+        int high = n - 1;
+        int ans = n;
+
+        while(low <= high) {
+
+            int mid = (low + high) / 2;
+
+            if(nums[mid] >= target) {
+                ans = mid;
+                high = mid - 1;
+            }
+            else {
+                low = mid + 1;
+            }
+        }
+
+        return ans;
     }
 };
