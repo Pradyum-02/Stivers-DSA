4c5e6aa0201a78355a9e9a3a43e4034df6e3956c
Pradyum Meshram <pradyumofc13@gmail.com>
2026-08-27 20:20:56 +0530

    Time: 0 ms (100%), Space: 8.4 MB (91.47%) - LeetHub

---
+++ 0392-is-subsequence/0392-is-subsequence.cpp
@@ -0,0 +1,16 @@
+// this is a copy solution main will be upadayed later 
+
+class Solution {
+public:
+    bool isSubsequence(string s, string t) {
+        int i = 0;
+        
+        for(int j = 0; j < t.size(); j++) {
+            if(i < s.size() && s[i] == t[j]) {
+                i++;
+            }
+        }
+        
+        return i == s.size();
+    }
+};
