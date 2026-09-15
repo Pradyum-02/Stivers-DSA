1e65a7a5c6537da804491313925f1698ba6be92b
Pradyum Meshram <pradyumofc13@gmail.com>
2026-09-15 23:32:13 +0530

    Time: 143 ms (54.19%), Space: 18.9 MB (60.73%) - LeetHub

--- 2472-maximum-number-of-non-overlapping-palindrome-substrings/2472-maximum-number-of-non-overlapping-palindrome-substrings.cpp
+++ 2472-maximum-number-of-non-overlapping-palindrome-substrings/2472-maximum-number-of-non-overlapping-palindrome-substrings.cpp
@@ -1,18 +1,30 @@
+// ChatGPT Version
+
 class Solution {
 public:
     int maxPalindromes(string s, int k) {
         int n = s.size(), ans = 0, last = -1;
+
+        // p[i][j] = true if s[i...j] is a palindrome
         vector<vector<bool>> p(n, vector<bool>(n));
 
-        for(int i=n-1;i>=0;i--)
-            for(int j=i;j<n;j++)
-                p[i][j] = s[i]==s[j] && (j-i<2 || p[i+1][j-1]);
+        // Build palindrome table
+        for(int i = n - 1; i >= 0; i--)
+            for(int j = i; j < n; j++)
+                p[i][j] = s[i] == s[j] &&
+                          (j - i < 2 || p[i + 1][j - 1]);
 
-        for(int j=0;j<n;j++)
-            for(int i=last+1;i<=j-k+1;i++)
+        // Find non-overlapping palindromes
+        // j = ending position of the palindrome
+        for(int j = 0; j < n; j++)
+            for(int i = last + 1; i <= j - k + 1; i++)
+
+                // If s[i...j] is a palindrome of length >= k
                 if(p[i][j]) {
                     ans++;
-                    last=j;
+
+                    // Next palindrome must start after j
+                    last = j;
                     break;
                 }
 
